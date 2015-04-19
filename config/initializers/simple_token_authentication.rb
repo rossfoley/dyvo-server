dc4fd646fb624ad2e579bf885812baedd6134ccd
Ross Foley <ross3141@gmail.com>
2015-04-19 00:37:27 -0400

    Fix error with token authentication headers

--- config/initializers/simple_token_authentication.rb
+++ config/initializers/simple_token_authentication.rb
@@ -1,4 +1,3 @@
 SimpleTokenAuthentication.configure do |config|
   config.sign_in_token = false
-  config.header_names = { user: { authentication_token: 'X-User-Token', email: 'X-User-Username' } }  
 end
